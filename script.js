0839017c716191fcb051f402fed388378cd5c42f
pardoronal2001 <135676805+pardoronal2001@users.noreply.github.com>
2023-09-28 19:13:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function registerUser() {
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    
+    if (!name || !email || !password) {
+        alert('Por favor, complete todos los campos.');
+       
+name = False
+email = False
+password = False
+ 
+return;
+
+}
+
+    
+    const userDataContainer = document.getElementById('userData');
+    userDataContainer.innerHTML = `
+        <h3>Datos del Usuario Registrado:</h3>
+        <p><strong>Nombre:</strong> ${name}</p>
+        <p><strong>Correo Electrónico:</strong> ${email}</p>
+        <p><strong>Contraseña:</strong> ${password}</p>
+    `;
+}
